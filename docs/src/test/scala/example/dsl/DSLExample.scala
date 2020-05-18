99f9e3360929e259715acf2777954c1625b318ac
Will Sargent <will.sargent@gmail.com>
2020-05-18 09:33:55 -0700

    reformat

--- docs/src/test/scala/example/dsl/DSLExample.scala
+++ docs/src/test/scala/example/dsl/DSLExample.scala
@@ -11,7 +11,8 @@ object DSLExample {
 
     val logger = LoggerFactory.getLogger
 
-    val winners = List(Winner(23, List(2, 45, 34, 23, 3, 5)), Winner(54, List(52, 3, 12, 11, 18, 22)))
+    val winners =
+      List(Winner(23, List(2, 45, 34, 23, 3, 5)), Winner(54, List(52, 3, 12, 11, 18, 22)))
     val lotto = Lotto(5, List(2, 45, 34, 23, 7, 5, 3), winners, None)
 
     logger.info("message {}", lotto)
@@ -22,22 +23,23 @@ object DSLExample {
   }
 
   object Winner {
-    implicit val toArgument: ToArgument[Winner] = ToArgument { w =>
-      Argument(w.asBObject)
-    }
+    implicit val toArgument: ToArgument[Winner] = ToArgument { w => Argument(w.asBObject) }
   }
 
-  case class Lotto(id: Long, winningNumbers: List[Int], winners: List[Winner], drawDate: Option[java.util.Date]) {
+  case class Lotto(
+      id: Long,
+      winningNumbers: List[Int],
+      winners: List[Winner],
+      drawDate: Option[java.util.Date]
+  ) {
     lazy val asBObject: BObject = "lotto" ->
-      ("lotto-id" -> id) ~
+      ("lotto-id"          -> id) ~
         ("winning-numbers" -> winningNumbers) ~
-        ("draw-date" -> drawDate.map(_.toString)) ~
-        ("winners" -> winners.map(w => w.asBObject))
+        ("draw-date"       -> drawDate.map(_.toString)) ~
+        ("winners"         -> winners.map(w => w.asBObject))
   }
 
   object Lotto {
-    implicit val toArgument: ToArgument[Lotto] = ToArgument { lotto =>
-      Argument(lotto.asBObject)
-    }
+    implicit val toArgument: ToArgument[Lotto] = ToArgument { lotto => Argument(lotto.asBObject) }
   }
 }
