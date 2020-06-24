192acde8a36735047cd25ecf1fe337a3f611fce0
Scala Steward <me@scala-steward.org>
2020-06-24 06:41:32 +0200

    Update scalatest to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.1.2"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0"
   val scalamock = "org.scalamock" %% "scalamock" % "4.4.0"
 
   val terseLogback = "0.16.1"
