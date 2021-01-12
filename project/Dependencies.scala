9e9b3ff9fd74b2b4a243ab764b4f687a4b0501a4
Scala Steward <me@scala-steward.org>
2021-01-12 19:19:04 +0100

    Update refined to 0.9.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   def refined(scalaVersion: String): ModuleID =
     scalaVersion match {
       case s if s.startsWith("2.11") => "eu.timepit" %% "refined" % "0.9.12"
-      case _                         => "eu.timepit" %% "refined" % "0.9.17"
+      case _                         => "eu.timepit" %% "refined" % "0.9.20"
     }
 
   lazy val logbackUniqueId        = "com.tersesystems.logback" % "logback-uniqueid-appender" % terseLogback
