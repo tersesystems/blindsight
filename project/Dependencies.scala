a3b609fab19538033bf622c92e6de1dc8455629b
Scala Steward <me@scala-steward.org>
2021-06-28 10:43:55 +0200

    Update securitybuilder to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,5 +39,5 @@ object Dependencies {
   lazy val cronScheduler          = "io.timeandspace"                  % "cron-scheduler"            % "0.1"
   lazy val scalaJava8Compat       = "org.scala-lang.modules"          %% "scala-java8-compat"        % "1.0.0"
   lazy val tweakFlow              = "com.twineworks"                   % "tweakflow"                 % "1.3.2"
-  lazy val securitybuilder        = "com.tersesystems.securitybuilder" % "securitybuilder"           % "1.0.0"
+  lazy val securitybuilder        = "com.tersesystems.securitybuilder" % "securitybuilder"           % "1.0.1"
 }
