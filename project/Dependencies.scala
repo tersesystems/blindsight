fc29673fee0638ec273c979b8bf01c956e9243c8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-31 19:54:47 +0100

    Update tweakflow to 1.3.3 (#310)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,6 +38,6 @@ object Dependencies {
   lazy val logstashLogbackEncoder = "net.logstash.logback"    % "logstash-logback-encoder" % "6.6"
   lazy val cronScheduler          = "io.timeandspace"         % "cron-scheduler"           % "0.1"
   lazy val scalaJava8Compat       = "org.scala-lang.modules" %% "scala-java8-compat"       % "1.0.0"
-  lazy val tweakFlow              = "com.twineworks"          % "tweakflow"                % "1.3.2"
+  lazy val tweakFlow              = "com.twineworks"          % "tweakflow"                % "1.3.3"
   lazy val securitybuilder = "com.tersesystems.securitybuilder" % "securitybuilder" % "1.0.1"
 }
