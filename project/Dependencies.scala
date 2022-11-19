930a5806e73f75aea874ed254968e5143967b168
Scala Steward <scala_steward@virtuslab.com>
2022-11-19 00:56:25 +0000

    Update logback-classic to 1.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
 
   lazy val logbackUniqueId = "com.tersesystems.logback" % "logback-uniqueid-appender" % terseLogback
   lazy val logbackTracing  = "com.tersesystems.logback" % "logback-tracing"           % terseLogback
-  lazy val logbackClassic  = "ch.qos.logback"           % "logback-classic"           % "1.2.11"
+  lazy val logbackClassic  = "ch.qos.logback"           % "logback-classic"           % "1.4.4"
   lazy val logstashLogbackEncoder = "net.logstash.logback"    % "logstash-logback-encoder" % "7.2"
   lazy val cronScheduler          = "io.timeandspace"         % "cron-scheduler"           % "0.1"
   lazy val scalaJava8Compat       = "org.scala-lang.modules" %% "scala-java8-compat"       % "1.0.2"
